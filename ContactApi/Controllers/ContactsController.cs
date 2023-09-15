f0330d6f49b67dfd2fb5dc8939b60918199b9e13
Hussam Alshayeb <hussamalshayeb01@gmail.com>
2023-09-15 18:39:20 +0300

    Get All Contacts Method

---
+++ ContactApi/Controllers/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using ContactApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
+
+        }
+    }
+}
